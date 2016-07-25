d6daad02d78c5355842958a7cef0e94e4f095e3e
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-07-24 23:01:03 -0500

    wip: sentence iterator

---
+++ lib/Renard/Curie/Language/Analysis/SentenceIterator.pm
@@ -0,0 +1,9 @@
+use Renard::Curie::Setup;
+package Renard::Curie::Language::Analysis::SentenceIterator;
+
+# TODO
+# - Extract the sentences for a given page.
+#   - The first complete sentence for a page.
+#   - Need to look at the last page
+
+1;
